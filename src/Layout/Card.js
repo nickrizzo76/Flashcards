0e63dadccae34b9e32d3fbf7dfef5452f3a16ddf
nickrizzo76 <nrizzo414@gmail.com>
2022-02-04 13:16:23 -0500

    Moves deck cards logic into Card.js

--- src/Layout/Card.js
+++ src/Layout/Card.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-function Card({ card, nextCardHandler }) {
+
+function Card({ cards }) {
   const [isFlipped, setIsFlipped] = useState(false);
   const [cardHasBeenStudied, setCardHasBeenStudied] = useState(false);
+  const [cardIndex, setCardIndex] = useState(0);
+  const history = useHistory();
 
-  if (!card) return null;
+  if (!cards) return null;
+
+  const card = cards[cardIndex];
   const content = isFlipped ? card.back : card.front;
 
   const flipHandler = () => {
@@ -12,24 +18,29 @@ function Card({ card, nextCardHandler }) {
     setIsFlipped(!isFlipped);
   };
 
-  const nextHandler = () => {
-      setIsFlipped(false);
-      setCardHasBeenStudied(false);
-      nextCardHandler();
+
+  const nextCardHandler = () => {
+    setIsFlipped(false);
+    setCardHasBeenStudied(false);
+    const nextCardIndex = cardIndex + 1;
+    if(nextCardIndex < cards.length) return setCardIndex(nextCardIndex);
+    const restartCards = window.confirm(`Restart cards?\n\nClick 'cancel' to return to the home page.`);
+    
+    if(restartCards) return setCardIndex(0);
+    history.push('/');
   }
 
   const nextButton = (
-    <button
-      onClick={nextHandler}
-      type="button"
-      className="btn btn-secondary"
-    >
+    <button onClick={nextCardHandler} type="button" className="btn btn-secondary">
       Next
     </button>
   );
 
   return (
     <>
+      <h5>
+        Card {cardIndex + 1} of {cards.length}
+      </h5>
       <p>{content}</p>
       <button onClick={flipHandler} type="button" className="btn btn-primary">
         Flip
--- src/Layout/DeckStudy.js
+++ src/Layout/DeckStudy.js
@@ -1,21 +1,11 @@
-import React, { useState } from "react";
-import { Link, useParams, useHistory } from "react-router-dom";
+import React from "react";
+import { useParams } from "react-router-dom";
 import Card from "./Card";
 
 function DeckStudy({ decks }) {
   const { deckId } = useParams();
-  const history = useHistory();
-  const deck = decks.find((deck) => deck.id === Number(deckId));
-  const [cardIndex, setCardIndex] = useState(0);
 
-  const nextCardHandler = () => {
-    const nextCardIndex = cardIndex + 1;
-    if(nextCardIndex < deck.cards.length) return setCardIndex(nextCardIndex);
-    const restartCards = window.confirm(`Restart cards?\n\nClick 'cancel' to return to the home page.`);
-    
-    if(restartCards) return setCardIndex(0);
-    history.push('/');
-  }
+  const deck = decks.find((deck) => deck.id === Number(deckId));
 
   if (!deck) {
     return null;
@@ -24,8 +14,7 @@ function DeckStudy({ decks }) {
   return (
     <>
       <h1>Study: {deck.name}</h1>
-      <h3>Number of cards: {deck.cards.length}</h3>
-      <Card card={deck.cards[cardIndex]} nextCardHandler={nextCardHandler}/>
+      <Card cards={deck.cards}/>
     </>
   );
 }
