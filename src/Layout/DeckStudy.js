008283d027b332cf726f9852625e4d5ff37b67cf
nickrizzo76 <nrizzo414@gmail.com>
2022-02-04 14:18:42 -0500

    Checks for minimum number of cards and displays message and link to add cards if needed

--- src/Layout/DeckStudy.js
+++ src/Layout/DeckStudy.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import Card from "./Card";
 
 function DeckStudy({ decks }) {
@@ -10,11 +10,26 @@ function DeckStudy({ decks }) {
   if (!deck) {
     return null;
   }
-
+  const minCards = 3;
+  if (deck.cards.length >= minCards) {
+    return (
+      <>
+        <h1>Study: {deck.name}</h1>
+        <Card cards={deck.cards} />
+      </>
+    );
+  }
   return (
     <>
-      <h1>Study: {deck.name}</h1>
-      <Card cards={deck.cards}/>
+      <h1>{deck.name}: Study</h1>
+      <h5>Not enough cards.</h5>
+      <p>You need at least {minCards} cards to study. There are {deck.cards.length} cards in this deck.</p>
+      
+      <Link to={`/decks/${deck.id}/cards/new`}>
+        <button type="button" className="btn btn-primary">
+          Add Cards
+        </button>
+      </Link>
     </>
   );
 }
